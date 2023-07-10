c6c045b60898a6dd33c7f4d5554c3139827f2948
LautaTuzzio <aerodactyl2006@gmail.com>
2023-07-10 15:58:29 -0300

    unidor de frases 3000

--- script.js
+++ script.js
@@ -1,94 +1,61 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var sobreMiButton = document.getElementById('sobre-mi-btn');
-    var containerDiv = document.querySelector('.container');
-  
-    sobreMiButton.addEventListener('click', function() {
-      var newDiv = document.createElement('div');
-      newDiv.className = 'about-container';
-      newDiv.innerHTML = `
-      <h1>Sobre mi</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      `;
-  
-      containerDiv.parentNode.replaceChild(newDiv, containerDiv);
-    });
-  });
+var phrases = [];
 
+function addPhrase(event) {
+    if (event.key === "Enter") {
+        var phraseInput = document.getElementById("phrase-input");
+        var phrase = phraseInput.value;
 
+        if (phrase.trim() !== "") {
+            phrases.push(phrase);
+            appendPhraseToList(phrase);
+        }
 
-  document.addEventListener('DOMContentLoaded', function() {
-  var temasButton = document.getElementById('temas-btn');
-  var body = document.body;
-  var currentTheme = 1;
-
-  temasButton.addEventListener('click', function() {
-    currentTheme = currentTheme < 4 ? currentTheme + 1 : 1;
-
-    body.classList.remove('theme1', 'theme2', 'theme3', 'theme4');
-
-    switch (currentTheme) {
-      case 1:
-        body.classList.add('theme1');
-        break;
-      case 2:
-        body.classList.add('theme2');
-        break;
-      case 3:
-        body.classList.add('theme3');
-        break;
-      case 4:
-        body.classList.add('theme4');
-        break;
-      default:
-        break;
+        phraseInput.value = "";
     }
-  });
-});
+}
 
-document.addEventListener('DOMContentLoaded', function() {
-    var temasButton = document.getElementById('temas-btn');
-    var body = document.body;
-    var enviarButton = document.querySelector('input[type="submit"]');
-    var currentTheme = 1;
-  
-    temasButton.addEventListener('click', function() {
-      currentTheme = currentTheme < 4 ? currentTheme + 1 : 1;
-  
-      body.classList.remove('theme1', 'theme2', 'theme3', 'theme4');
-  
-      switch (currentTheme) {
-        case 1:
-          body.classList.add('theme1');
-          enviarButton.className = '';
-          break;
-        case 2:
-          body.classList.add('theme2');
-          enviarButton.className = 'theme2';
-          break;
-        case 3:
-          body.classList.add('theme3');
-          enviarButton.className = 'theme3';
-          break;
-        case 4:
-          body.classList.add('theme4');
-          enviarButton.className = 'theme4';
-          break;
-        default:
-          break;
-      }
-    });
-  });
-  
-  function refreshPage() {
-    location.reload();
-  }
-  
+function mergePhrases() {
+    var phraseList = document.getElementById("phrase-list");
+    var mergedPhraseDiv = document.getElementById("merged-phrase");
 
-  function downloadFile() {
-    var link = document.createElement('a');
-    link.setAttribute('href', 'Codigo.zip');
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  }
-  
+    if (phrases.length < 2) {
+        alert("Por favor, escribe frases antes de tocar el botón");
+        return;
+    }
+
+    var randomIndex1 = Math.floor(Math.random() * phrases.length);
+    var randomIndex2 = Math.floor(Math.random() * phrases.length);
+
+    while (randomIndex1 === randomIndex2) {
+        randomIndex2 = Math.floor(Math.random() * phrases.length);
+    }
+
+    var phrase1 = phrases[randomIndex1];
+    var phrase2 = phrases[randomIndex2];
+
+    var firstHalfPhrase1 = splitPhrase(phrase1).firstHalf;
+    var secondHalfPhrase2 = splitPhrase(phrase2).secondHalf;
+
+    var mergedPhrase = firstHalfPhrase1 + " " + secondHalfPhrase2;
+
+    mergedPhraseDiv.textContent = mergedPhrase;
+}
+
+function splitPhrase(phrase) {
+    var words = phrase.trim().split(" ");
+    var mid = Math.floor(words.length / 2);
+    var firstHalf = words.slice(0, mid).join(" ");
+    var secondHalf = words.slice(mid).join(" ");
+
+    return {
+        firstHalf: firstHalf,
+        secondHalf: secondHalf
+    };
+}
+
+function appendPhraseToList(phrase) {
+    var phraseList = document.getElementById("phrase-list");
+    var listItem = document.createElement("li");
+    listItem.textContent = phrase;
+    phraseList.appendChild(listItem);
+}
