183b43e7d8eda7719b6a81c5f01e91bd66f25c2d
LautaTuzzio <aerodactyl2006@gmail.com>
2023-07-10 16:12:36 -0300

    nuevo php

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <?php
 
-$db = new PDO("mysql:host=localhost;dbname=mydb;charset=utf8mb4", "root", "");
+// Create a database connection
+$db = new PDO("mysql:host=localhost;dbname=frases;charset=utf8mb4", "root", "");
 
+// Get the phrases from the input
 $phrases = $_POST["phrases"];
 
+// Insert the phrases into the database
 foreach ($phrases as $phrase) {
   $sql = "INSERT INTO phrases (phrase) VALUES ('$phrase')";
   $db->query($sql);
 }
+
+// Merge the phrases and display them on the page
+$merged_phrase = implode(" ", $phrases);
+echo $merged_phrase;
 
 ?>
