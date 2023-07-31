a7e57631e4b130108a1a428bacf4f580e42b3514
LautaTuzzio <aerodactyl2006@gmail.com>
2023-07-31 15:41:29 -0300

    Web terminada

--- script.js
+++ script.js
@@ -1,61 +1,138 @@
 var phrases = [];
 
 function addPhrase(event) {
-    if (event.key === "Enter") {
-        var phraseInput = document.getElementById("phrase-input");
-        var phrase = phraseInput.value;
+  if (event.key === "Enter") {
+    var phraseInput = document.getElementById("phrase-input");
+    var phrase = phraseInput.value;
 
-        if (phrase.trim() !== "") {
-            phrases.push(phrase);
-            appendPhraseToList(phrase);
-        }
-
-        phraseInput.value = "";
+    if (phrase.trim() !== "" && isValidPhrase(phrase)) {
+      phrases.push(phrase);
+      appendPhraseToList(phrase);
+      savePhrasesToLocalStorage();
+    } else {
+      alert("Por favor, Ingrese una frase valida");
     }
+
+    phraseInput.value = "";
+  }
+}
+
+function isValidPhrase(phrase) {
+  var commaIndex = phrase.indexOf(",");
+  if (commaIndex === -1 || commaIndex === 0 || commaIndex === phrase.length - 1) {
+    return false;
+  }
+  return true;
 }
 
 function mergePhrases() {
-    var phraseList = document.getElementById("phrase-list");
-    var mergedPhraseDiv = document.getElementById("merged-phrase");
+  var phraseList = document.getElementById("phrase-list");
+  var mergedPhraseDiv = document.getElementById("merged-phrase");
 
-    if (phrases.length < 2) {
-        alert("Por favor, escribe frases antes de tocar el botón");
-        return;
-    }
+  if (phrases.length < 2) {
+    alert("Por favor, escribe frases antes de tocar el botón");
+    return;
+  }
 
-    var randomIndex1 = Math.floor(Math.random() * phrases.length);
-    var randomIndex2 = Math.floor(Math.random() * phrases.length);
+  if (localStorage.getItem("phrases")) {
+    phrases = JSON.parse(localStorage.getItem("phrases"));
+  }
 
-    while (randomIndex1 === randomIndex2) {
-        randomIndex2 = Math.floor(Math.random() * phrases.length);
-    }
+  var randomIndex1 = Math.floor(Math.random() * phrases.length);
+  var randomIndex2 = Math.floor(Math.random() * phrases.length);
 
-    var phrase1 = phrases[randomIndex1];
-    var phrase2 = phrases[randomIndex2];
+  while (randomIndex1 === randomIndex2) {
+    randomIndex2 = Math.floor(Math.random() * phrases.length);
+  }
 
-    var firstHalfPhrase1 = splitPhrase(phrase1).firstHalf;
-    var secondHalfPhrase2 = splitPhrase(phrase2).secondHalf;
+  var phrase1 = phrases[randomIndex1];
+  var phrase2 = phrases[randomIndex2];
 
-    var mergedPhrase = firstHalfPhrase1 + " " + secondHalfPhrase2;
+  var firstHalfPhrase1 = splitPhrase(phrase1).firstHalf;
+  var secondHalfPhrase2 = splitPhrase(phrase2).secondHalf;
 
-    mergedPhraseDiv.textContent = mergedPhrase;
+  var mergedPhrase = firstHalfPhrase1 + ", " + secondHalfPhrase2;
+
+  mergedPhraseDiv.textContent = mergedPhrase;
+
+  savePhrasesToLocalStorage();
 }
 
 function splitPhrase(phrase) {
-    var words = phrase.trim().split(" ");
-    var mid = Math.floor(words.length / 2);
-    var firstHalf = words.slice(0, mid).join(" ");
-    var secondHalf = words.slice(mid).join(" ");
-
+  var commaIndex = phrase.indexOf(",");
+  if (commaIndex === -1) {
     return {
-        firstHalf: firstHalf,
-        secondHalf: secondHalf
+      firstHalf: phrase.trim(),
+      secondHalf: ""
     };
+  }
+
+  var firstHalf = phrase.substring(0, commaIndex).trim();
+  var secondHalf = phrase.substring(commaIndex + 1).trim();
+
+  return {
+    firstHalf: firstHalf,
+    secondHalf: secondHalf
+  };
 }
 
 function appendPhraseToList(phrase) {
-    var phraseList = document.getElementById("phrase-list");
-    var listItem = document.createElement("li");
-    listItem.textContent = phrase;
-    phraseList.appendChild(listItem);
+  var phraseList = document.getElementById("phrase-list");
+  var listItem = document.createElement("li");
+  listItem.textContent = phrase;
+  phraseList.appendChild(listItem);
 }
+
+function savePhrasesToLocalStorage() {
+  localStorage.setItem("phrases", JSON.stringify(phrases));
+}
+
+function deleteList() {
+  phrases = [];
+  savePhrasesToLocalStorage();
+  clearPhraseList();
+}
+
+function deleteNextPhrase(event) {
+  if (deleteMode && event.target.tagName === "LI") {
+    var phraseToDelete = event.target.textContent;
+    deletePhrase(phraseToDelete);
+    deleteMode = false;
+    var deleteButton = document.getElementById("delete-list-button");
+    deleteButton.textContent = "Borrar Frase";
+  }
+}
+
+function deletePhrase(phraseToDelete) {
+  var index = phrases.indexOf(phraseToDelete);
+  if (index !== -1) {
+    phrases.splice(index, 1);
+    savePhrasesToLocalStorage();
+    clearPhraseList();
+    phrases.forEach(function (phrase) {
+      appendPhraseToList(phrase);
+    });
+  }
+}
+
+function clearPhraseList() {
+  var phraseList = document.getElementById("phrase-list");
+  phraseList.innerHTML = "";
+}
+
+var deleteMode = false;
+
+function deleteList() {
+  deleteMode = !deleteMode;
+  var deleteButton = document.getElementById("delete-list-button");
+  deleteButton.textContent = deleteMode ? "Cancelar" : "Borrar Frase";
+}
+
+window.addEventListener("load", function () {
+  if (localStorage.getItem("phrases")) {
+    phrases = JSON.parse(localStorage.getItem("phrases"));
+    phrases.forEach(function (phrase) {
+      appendPhraseToList(phrase);
+    });
+  }
+});
