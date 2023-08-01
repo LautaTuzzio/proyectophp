96f1cba4d473b53152a20726bc08193394391a43
LautaTuzzio <aerodactyl2006@gmail.com>
2023-08-01 19:33:29 +0000

    web finito

--- script.js
+++ script.js
@@ -136,3 +136,18 @@ window.addEventListener("load", function () {
     });
   }
 });
+
+const showDivsButton = document.getElementById("showDivsButton");
+const divContainer = document.getElementById("divContainer");
+const closeButton = document.getElementById("closeButton");
+
+showDivsButton.addEventListener("click", function () {
+  divContainer.style.display = "flex";
+  showDivsButton.style.display = "none";
+});
+
+closeButton.addEventListener("click", function () {
+  divContainer.style.display = "none";
+  showDivsButton.style.display = "block";
+});
+
